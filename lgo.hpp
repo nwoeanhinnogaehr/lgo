32f57e27a17df70dbce9eea4b4c1c01ad362fbbb
Noah Weninger <nweninge@ualberta.ca>
2016-12-25 15:13:22 -0700

    Implement scoring, closes #4

--- lgo.hpp
+++ lgo.hpp
@@ -17,13 +17,54 @@ struct Move {
     bool pass;
 };
 
+struct Score {
+    Score() {}
+    Score(const pos_t black, const pos_t white) : black(black), white(white) {}
+    pos_t black = 0, white = 0;
+    bool operator==(const Score o) const { return o.black == black && o.white == white; }
+};
+
 struct Board {
+    Board(pos_t size) : size(size) {}
+    Board(std::initializer_list<Cell> init) : size(init.size()) {
+        for (auto it = init.begin(); it != init.end(); ++it)
+            set(it - init.begin(), *it);
+    }
+    pos_t size;
     pos_t board = 0;
     inline Cell get(const pos_t pos) const { return Cell(board >> pos * CELL_WIDTH & CELL_MAX); }
     inline void set(const pos_t pos, const Cell color) {
         board ^= board & CELL_MAX << pos * CELL_WIDTH ^ color << pos * CELL_WIDTH;
     }
+    Score score() const {
+        Score sc;
+        Board left_smear(size), right_smear(size);
+        Cell last_left = EMPTY, last_right = EMPTY;
+        for (pos_t i = 0; i < size; i++) {
+            if (Cell next = get(i))
+                last_right = next;
+            right_smear.set(i, last_right);
+            if (Cell next = get(size - i - 1))
+                last_left = next;
+            left_smear.set(size - i - 1, last_left);
+        }
+        for (pos_t i = 0; i < size; i++) {
+            Cell left = left_smear.get(i), right = right_smear.get(i);
+            if (left == BLACK && (left == right || right == EMPTY) ||
+                right == BLACK && left == EMPTY)
+                sc.black++;
+            if (left == WHITE && (left == right || right == EMPTY) ||
+                right == WHITE && left == EMPTY)
+                sc.white++;
+        }
+        return sc;
+    }
     bool operator==(const Board o) const { return o.board == board; }
+    friend std::ostream& operator << ( std::ostream& os, const Board board ) {
+        for (pos_t i = 0; i < board.size; i++)
+            os << ".BW"[board.get(i)];
+        return os;
+    }
 };
 
 struct BoardHasher {
@@ -37,6 +78,7 @@ struct History {
 };
 
 struct State {
+    State(pos_t size) : board(size) {}
     Board board;
     History history;
     enum GameState { NORMAL, PASS, GAME_OVER } game_state = NORMAL;
--- lgotest.cpp
+++ lgotest.cpp
@@ -3,7 +3,7 @@
 
 TEST_CASE("Board get/set works properly", "[board_basic]") {
     for (pos_t i = 0; i < MAX_SIZE; i++) {
-        Board s;
+        Board s(MAX_SIZE);
         for (pos_t c = 0; c < CELL_MAX; c++) {
             s.set(i, Cell(c));
             REQUIRE(s.get(i) == c);
@@ -17,7 +17,7 @@ TEST_CASE("Board get/set works properly", "[board_basic]") {
 
 TEST_CASE("History works", "[history]") {
     History h;
-    Board s;
+    Board s(MAX_SIZE);
     for (pos_t i = 0; i < MAX_SIZE; i++) {
         for (pos_t c = 1; c < CELL_MAX; c++) {
             s.set(i, Cell(c));
@@ -29,7 +29,7 @@ TEST_CASE("History works", "[history]") {
 }
 
 TEST_CASE("Passing twice results in a terminal state", "[state_pass]") {
-    State s;
+    State s(MAX_SIZE);
     REQUIRE(!s.terminal());
     s.play(Move(BLACK, 0, false));
     REQUIRE(!s.terminal());
@@ -41,4 +41,70 @@ TEST_CASE("Passing twice results in a terminal state", "[state_pass]") {
     REQUIRE(!s.terminal());
     s.play(Move(BLACK, 0, true));
     REQUIRE(s.terminal());
+}
+
+namespace Catch {
+template <> struct StringMaker<Score> {
+    static std::string convert(Score const &value) {
+        return "{ B+" + std::to_string(value.black) + ", W+" + std::to_string(value.white) + " }";
+    }
+};
+}
+
+TEST_CASE("Board scoring size 1", "[score_board_1]") {
+    Board b(1);
+    REQUIRE(b.score() == Score(0, 0));
+    b.set(0, BLACK);
+    REQUIRE(b.score() == Score(1, 0));
+    b.set(0, WHITE);
+    REQUIRE(b.score() == Score(0, 1));
+}
+
+TEST_CASE("Board scoring size 2", "[score_board_2]") {
+    Board b(2);
+    REQUIRE(b.score() == Score(0, 0));
+    b.set(0, BLACK);
+    REQUIRE(b.score() == Score(2, 0));
+    b.set(1, WHITE);
+    REQUIRE(b.score() == Score(1, 1));
+    b.set(0, WHITE);
+    REQUIRE(b.score() == Score(0, 2));
+    b.set(1, BLACK);
+    REQUIRE(b.score() == Score(1, 1));
+    b.set(1, EMPTY);
+    REQUIRE(b.score() == Score(0, 2));
+}
+
+TEST_CASE("Board scoring size 3", "[score_board_3]") {
+    Board b(3);
+    REQUIRE(b.score() == Score(0, 0));
+    b.set(1, BLACK);
+    REQUIRE(b.score() == Score(3, 0));
+    b.set(0, BLACK);
+    REQUIRE(b.score() == Score(3, 0));
+    b.set(1, WHITE);
+    REQUIRE(b.score() == Score(1, 2));
+    b.set(0, WHITE);
+    REQUIRE(b.score() == Score(0, 3));
+    b.set(2, BLACK);
+    REQUIRE(b.score() == Score(1, 2));
+    b.set(1, EMPTY);
+    REQUIRE(b.score() == Score(1, 1));
+}
+
+TEST_CASE("Board scoring size 4", "[score_board_4]") {
+    Board b(4);
+    REQUIRE(b.score() == Score(0, 0));
+    b.set(1, BLACK);
+    REQUIRE(b.score() == Score(4, 0));
+    b.set(2, WHITE);
+    REQUIRE(b.score() == Score(2, 2));
+    b.set(1, WHITE);
+    REQUIRE(b.score() == Score(0, 4));
+    b.set(0, BLACK);
+    REQUIRE(b.score() == Score(1, 3));
+    b.set(3, BLACK);
+    REQUIRE(b.score() == Score(2, 2));
+    b.set(1, EMPTY);
+    REQUIRE(b.score() == Score(2, 1));
 }
