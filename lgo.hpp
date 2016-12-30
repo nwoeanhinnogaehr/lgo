e18335693f425d93a0b5c037f49e89dacb490daf
Noah Weninger <nweninge@ualberta.ca>
2016-12-29 17:38:53 -0700

    Enhanced cell 2 conjecture unfortunately appears to perform worse

--- lgo.hpp
+++ lgo.hpp
@@ -160,7 +160,7 @@ template <pos_t size> struct Board {
     }
     bool operator==(Board o) const { return o.board == board; }
     friend std::ostream &operator<<(std::ostream &os, Board board) {
-        for (pos_t i = 0; i < board.size; i++)
+        for (pos_t i = 0; i < size; i++)
             os << board.get(i);
         return os;
     }
@@ -252,10 +252,9 @@ template <pos_t size> struct State {
             }
         }
     }
-    void cell_2_conjecture(Cell color, pos_t &legal, std::vector<Move> &moves) const {
+    void cell_2_conjecture_simple(Cell color, pos_t &legal, std::vector<Move> &moves) const {
         if (size < 4)
             return;
-        // simple for now
         if ((legal & 3) == 3) {
             moves.emplace_back(color, 1);
             legal &= ~2;
@@ -263,6 +262,20 @@ template <pos_t size> struct State {
         if ((legal & (3 << (size - 2))) == (3 << (size - 2))) {
             moves.emplace_back(color, size - 2);
             legal &= ~(1 << (size - 2));
+        }
+    }
+    void cell_2_conjecture_full(Cell color, pos_t &legal, std::vector<Move> &moves) const {
+        if (size < 4)
+            return;
+        for (pos_t i = 0; i < size - 2; i += 2) {
+            if ((legal & (3 << i)) == 3_pos_t << i) {
+                moves.emplace_back(color, i + 1);
+                legal &= ~(2 << i);
+            }
+            if ((legal & (3 << (size - i - 2))) == (3_pos_t << (size - i - 2))) {
+                moves.emplace_back(color, size - i - 2);
+                legal &= ~(1 << (size - i - 2));
+            }
         }
     }
     void safe_moves(Cell color, pos_t &legal, std::vector<Move> &moves) const {}
@@ -273,9 +286,11 @@ template <pos_t size> struct State {
                 moves.emplace_back(color, i);
     }
     void moves(Cell color, std::vector<Move> &moves) const {
+        moves.clear();
         pos_t legal = legal_moves(color);
         moves.emplace_back(color); // pass
-        cell_2_conjecture(color, legal, moves);
+        cell_2_conjecture_simple(color, legal, moves);
+        //cell_2_conjecture_full(color, legal, moves);
         atari_moves(color, legal, moves);
         safe_moves(color, legal, moves);
         other_moves(color, legal, moves);
