84fc04d94fd11e65b3b05cd744ca92cdd454b632
Noah Weninger <nweninge@ualberta.ca>
2017-01-29 13:00:37 -0700

    Prove multiple conjectures at once

--- conjecture.hpp
+++ conjecture.hpp
@@ -3,6 +3,17 @@
 
 using std::experimental::optional;
 
+// helper to build a class hierarchy from a variadic list of types
+template <template <pos_t, typename, template <pos_t, typename, typename> typename>
+          typename Interior,
+          pos_t size, typename Impl, template <pos_t, typename, typename> typename First,
+          template <pos_t, typename, typename> typename... Rest>
+struct ManyImplWrapper : Interior<size, ManyImplWrapper<Interior, size, Impl, Rest...>, First> {};
+template <template <pos_t, typename, template <pos_t, typename, typename> typename>
+          typename Interior,
+          pos_t size, typename Impl, template <pos_t, typename, typename> typename First>
+struct ManyImplWrapper<Interior, size, Impl, First> : Interior<size, Impl, First> {};
+
 // search for nodes which conflict with conjecture. example usage:
 // ConjectureProver<size, Minimax<size>, AlphaBeta, NullPruningConjecture> prover;
 // State<size> state;
@@ -22,18 +33,19 @@ struct ConjectureProverImplWrapper : Impl {
     }
 };
 template <pos_t size, typename Impl, template <pos_t, typename> typename Search,
-          template <pos_t, typename, typename> typename Conjecture>
-struct ConjectureProver : Search<size, ConjectureProverImplWrapper<size, Impl, Conjecture>> {};
+          template <pos_t, typename, typename> typename... Conjectures>
+struct ConjectureProver
+    : Search<size, ManyImplWrapper<ConjectureProverImplWrapper, size, Impl, Conjectures...>> {};
 
 // do search using pruning as specified by conjecture(s). example usage:
 // PrunedSearch<size, Minimax<size>, AlphaBeta, PruningConjecture1, PruningConjecture2, ...> solver;
 // State<size> state;
 // solver.search(state);
-template <pos_t size, typename Impl, template <pos_t, typename, typename> typename First>
-struct PrunedSearchImplWrapperImpl : Impl {
+template <pos_t size, typename Impl, template <pos_t, typename, typename> typename Conjecture>
+struct PrunedSearchImplWrapper : Impl {
     typedef typename Impl::minimax_t minimax_t;
     typedef typename Impl::return_t return_t;
-    First<size, minimax_t, return_t> conj;
+    Conjecture<size, minimax_t, return_t> conj;
     return_t on_enter(State<size> &state, minimax_t &alpha, minimax_t &beta, size_t depth,
                       bool &terminal) {
         if (optional<return_t> conjected = conj.apply(state, alpha, beta)) {
@@ -43,20 +55,10 @@ struct PrunedSearchImplWrapperImpl : Impl {
         return Impl::on_enter(state, alpha, beta, depth, terminal);
     }
 };
-template <pos_t size, typename Impl, template <pos_t, typename, typename> typename First,
-          template <pos_t, typename, typename> typename... Rest>
-struct PrunedSearchImplWrapper
-    : PrunedSearchImplWrapperImpl<size, PrunedSearchImplWrapper<size, Impl, Rest...>, First> {};
-template <pos_t size, typename Impl, template <pos_t, typename, typename> typename First>
-struct PrunedSearchImplWrapper<size, Impl, First> : PrunedSearchImplWrapperImpl<size, Impl, First> {
-};
 template <pos_t size, typename Impl, template <pos_t, typename> typename Search,
           template <pos_t, typename, typename> typename... Prunes>
-struct PrunedSearch {
-    using ImplWrapper = PrunedSearchImplWrapper<size, Impl, Prunes...>;
-    Search<size, ImplWrapper> impl;
-    typename ImplWrapper::return_t search(State<size> &state) { return impl.search(state); }
-};
+struct PrunedSearch
+    : Search<size, ManyImplWrapper<PrunedSearchImplWrapper, size, Impl, Prunes...>> {};
 
 // conjectures below
 template <pos_t size, typename minimax_t, typename return_t> struct NullPruningConjecture {
