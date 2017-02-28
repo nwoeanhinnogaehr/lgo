3697a3a924cfff32834fb2799050ad09ef6f0f62
Noah Weninger <nweninge@ualberta.ca>
2017-02-28 15:44:38 -0700

    Disable stability conjecture for size >= 8

--- conjectures.hpp
+++ conjectures.hpp
@@ -2,6 +2,7 @@
 #include "conjectures/stability.hpp"
 
 namespace conjectures {
-template <pos_t size, typename Impl>
-using All = Stability<size, Full<size, Impl>>;
+template <pos_t size, typename Impl, typename = void> struct All;
+template <pos_t size, typename Impl> struct All<size, Impl, std::enable_if_t<(size < 8)>> : Stability<size, Full<size, Impl>> { };
+template <pos_t size, typename Impl> struct All<size, Impl, std::enable_if_t<(size >= 8)>> : Full<size, Impl> { };
 };
